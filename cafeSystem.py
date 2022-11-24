1b524b01fb7d048fd14c013c63d25af177777286
Eeap <kdg97811@naver.com>
2022-11-25 01:49:56 +0900

    feat: apply fastapi & add comment

--- cafeSystem.py
+++ cafeSystem.py
@@ -16,53 +16,59 @@ class cafeSystem:
         self.photoType = '.cont_menu > .list_menu > .photo_type'
         self.menuName = '.info_menu > .loss_word'
         self.menuPrice = '.info_menu > .price_menu'
-        self.dataList = '아메리카노|카페|라떼|스무디|주스|쥬스|티|차|콜드|아이스|요구르트|브루|에스프레소|모카|ICE|HOT|얼그레이|프라푸치노|커피|블렌디드|핫|카푸치노|돌체|요거트'
+        self.dataList = '아메리카노|카페|라떼|스무디|주스|쥬스|티|차|콜드' \
+                        '|아이스|요구르트|브루|에스프레소|모카|ICE|HOT|얼그레이' \
+                        '|프라푸치노|커피|블렌디드|핫|카푸치노|돌체|요거트|시그니처|카라멜|에이드|마끼아또'
         pd.set_option('display.max_rows', None)
         pd.set_option('display.max_columns', 4)
+    # 장소를 입력하는 함수
     def dataInput(self):
         place = input()
         return place
-
+    #검색을 돌리기 위한 메인 함수
     def searchMain(self, place):
+        #크롬 드라이버가 돌아가는걸 4초 정도 기다림
         self.driver.implicitly_wait(4)
         self.driver.get('https://map.kakao.com/')
         query = place + " 카페"
         path = os.path.join(os.getcwd()+"/data/", query + '.csv')
         filename = os.path.join(os.getcwd()+"/data/",query + ".csv")
+        #파일이 이미 존재하는 경우 재크롤링을 하지 않음
         if os.path.exists(path):
-            printData = self.data_read(filename)
+            printData = self.dataRead(filename)
             self.driver.quit()
             return printData
-            # csv읽어서 정렬해서 출력
         else:
+            #검색 후 저장된 데이터를 출력
             self.search(query,filename)
-            printData = self.data_read(filename)
+            printData = self.dataRead(filename)
             self.driver.quit()
             return printData
-
-    def data_read(self, filename):
+    #csv파일의 데이터를 읽어오는 함수
+    def dataRead(self, filename):
         data = pd.read_csv(filename)
         sortData = data.sort_values(by='price')
         printData = sortData[sortData['menu'].str.contains(self.dataList)]
         return printData
-
+    #실제로 검색과 저장을 하는 함수
     def search(self, query,filename):
         f = open(filename, 'w')
         filewrite = csv.writer(f)
         filewrite.writerow(['menu', 'price', 'title','url'])
+        #해당 장소 카페 검색
         searchQuery = self.driver.find_element(By.XPATH, self.queryXpath)
         searchQuery.send_keys(query)
         self.driver.find_element(By.XPATH, self.submitXpath).send_keys(Keys.ENTER)
-
-        sleep(3)  # wait for searching
-
+        #검색 해오는 걸 3초 정도 기다림
+        sleep(3)
+        #페이지 중 검색된 카페 리스트를 가져와서 lists에 저장
         page = self.driver.page_source
         soup = BeautifulSoup(page, "html.parser")
         lists = soup.select(self.placeList)
 
         self.getData(lists, filewrite)
         searchQuery.clear()
-
+        #첫번째 페이지 이후의 페이지의 데이터를 크롤링(2~5페이지)
         try:
             self.driver.find_element(By.XPATH, '//*[@id="info.search.place.more"]').send_keys(Keys.ENTER)
             sleep(2)
@@ -81,20 +87,23 @@ class cafeSystem:
             searchQuery.clear()
         f.close()
 
+    #페이지에 있는 카페 리스트 중 하나씩 꺼내서 데이터를 크롤링하는 함수
     def getData(self, lists, filewrite):
         for idx, query in enumerate(lists):
             self.getMenu(idx, filewrite)
-
+    #실제로 카페 정보를 크롤링하는 함수
     def getMenu(self, idx, filewrite):
+        #상세 페이지로 넘어감()
         pageXpath = '//*[@id="info.search.place.list"]/li[' + str(idx + 1) + ']/div[5]/div[4]/a[1]'
         self.driver.find_element(By.XPATH, pageXpath).send_keys(Keys.ENTER)
         self.driver.switch_to.window(self.driver.window_handles[-1])
-        sleep(3)
+        sleep(3) #페이지 로딩 3초 정도 기다림
         menu = []
         page = self.driver.page_source
         soup = BeautifulSoup(page, "html.parser")
         title = soup.find('title').text.split("|")[0].strip()
         url = soup.head.find("meta",{"property":"og:url"}).get("content")
+        #카카오맵의 menu에 대한 타입 세가지 중 데이터가 있는 타입의 데이터를 가져옴
         menuItem = soup.select(self.menuType)
         nophotoItem = soup.select(self.nophotoType)
         photoItem = soup.select(self.photoType)
@@ -109,6 +118,10 @@ class cafeSystem:
                 menu.append(self.parseMenu(v))
         if (len(menu) > 0):
             for item in menu:
+                '''
+                가격의 경우 빈값도 오고 단위가 1,000처럼 ,가 들어오는 부분이 있어서 
+                데이터를 1000과 같이 int로 변환되게 바꿈
+                '''
                 price = ""
                 if ',' in item[1]:
                     temp = item[1].split(',')
@@ -119,17 +132,18 @@ class cafeSystem:
                 if price:
                     if self.isInt(price):
                         print(price, item[0])
+                        #데이터를 csv에 저장
                         filewrite.writerow([item[0], int(price), title, url])
         self.driver.close()
         self.driver.switch_to.window(self.driver.window_handles[0])
-
+    #메뉴와 가격의 데이터를 가져오는 함수
     def parseMenu(self, value):
         name = value.select(self.menuName)[0].text
         price = value.select(self.menuPrice)
         if len(price) != 0:
             price = price[0].text.split(" ")[1]
         return [name, price]
-
+    #정수형으로 변환할 수 있는지 확인하는 함수
     def isInt(self, price):
         try:
             int(price)
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
+import json
 import os
-from pprint import pprint
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from cafeSystem import cafeSystem
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-
+app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+#chrome 옵션 설정
 def makeOptions(webdriver):
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
     options.add_argument('lang=ko_KR')
-    options.add_argument('headless')
     return options
 
-if __name__=="__main__":
+app = FastAPI()
+@app.get("/")
+async def mainPage(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+@app.get("/get")
+async def getData(request: Request,place:str):
     chromedriver_path = "chromedriver"
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "none"
     cafe = cafeSystem(webdriver.Chrome(os.path.join(os.getcwd(), chromedriver_path), options= makeOptions(webdriver)))
-    place = cafe.dataInput()
-    data = cafe.searchMain(place)
-    pprint(data)
+    # place = cafe.dataInput()
+    result = cafe.searchMain(place)
+    data_list = pd.DataFrame(result, columns=['menu','price','title','url']).to_json(force_ascii=False)
+    return templates.TemplateResponse("cafe.html",{"request":request,
+                                                   "data":json.loads(data_list),
+                                                   "size":len(result)})
